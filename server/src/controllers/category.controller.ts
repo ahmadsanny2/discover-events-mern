5edbe6d459c0f5b67c4433a779131c0559f7d2d8
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-03-25 15:59:34 +0700

    refactor: tidy up the code

--- server/src/controllers/category.controller.ts
+++ server/src/controllers/category.controller.ts
@@ -6,13 +6,12 @@ import response from "../utils/response";
 export default {
     async create(req: IReqUser, res: Response) {
         try {
-            await categoryDAO.validate(req.body)
-            const result = await CategoryModel.create(req.body)
+            await categoryDAO.validate(req.body);
+            const result = await CategoryModel.create(req.body);
 
-            response.success(res, result, 'Category created successfully')
-
+            response.success(res, result, "Category created successfully");
         } catch (error) {
-            response.error(res, error, 'Failed create category')
+            response.error(res, error, "Failed create category");
         }
     },
 
@@ -20,11 +19,11 @@ export default {
         const {
             page = 1,
             limit = 10,
-            search
-        } = req.query as unknown as IPaginationQuery
+            search,
+        } = req.query as unknown as IPaginationQuery;
 
         try {
-            const query = {}
+            const query = {};
 
             if (search) {
                 Object.assign(query, {
@@ -34,65 +33,68 @@ export default {
                         },
                         {
                             description: { $reqex: search, $options: "i" },
-                        }
-                    ]
-                })
+                        },
+                    ],
+                });
             }
 
             const result = await CategoryModel.find(query)
                 .limit(limit)
                 .skip((page - 1) * limit)
                 .sort({ createdAt: -1 })
-                .exec()
+                .exec();
 
-            const count = await CategoryModel.countDocuments(query)
+            const count = await CategoryModel.countDocuments(query);
 
-            response.pagination(res, result, {
-                total: count,
-                totalPages: Math.ceil(count / limit),
-                current: page
-            }, 'Success find all category')
-
+            response.pagination(
+                res,
+                result,
+                {
+                    total: count,
+                    totalPages: Math.ceil(count / limit),
+                    current: page,
+                },
+                "Success find all category",
+            );
         } catch (error) {
-            response.error(res, error, 'Failed findAll category')
+            response.error(res, error, "Failed findAll category");
         }
     },
 
     async findOne(req: IReqUser, res: Response) {
         try {
-            const { id } = req.params
-            const result = await CategoryModel.findById(id)
+            const { id } = req.params;
+            const result = await CategoryModel.findById(id);
 
-            response.success(res, result, 'Success find one category')
-
+            response.success(res, result, "Success find one category");
         } catch (error) {
-            response.error(res, error, 'Failed findOne category')
+            response.error(res, error, "Failed findOne category");
         }
     },
 
     async update(req: IReqUser, res: Response) {
         try {
-            const { id } = req.params
+            const { id } = req.params;
             const result = await CategoryModel.findByIdAndUpdate(id, req.body, {
-                new: true
-            })
+                new: true,
+            });
 
-            response.success(res, result, 'Success update category')
-
+            response.success(res, result, "Success update category");
         } catch (error) {
-            response.error(res, error, 'Failed update category')
+            response.error(res, error, "Failed update category");
         }
     },
 
     async remove(req: IReqUser, res: Response) {
         try {
-            const { id } = req.params
-            const result = await CategoryModel.findByIdAndDelete(id, req.body)
+            const { id } = req.params;
+            const result = await CategoryModel.findByIdAndDelete(id, {
+                new: true,
+            });
 
-            response.success(res, result, 'Success deleted category')
-
+            response.success(res, result, "Success deleted category");
         } catch (error) {
-            response.error(res, error, 'Failed remove category')
+            response.error(res, error, "Failed remove category");
         }
     },
 };
