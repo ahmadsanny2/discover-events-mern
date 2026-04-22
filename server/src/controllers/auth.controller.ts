2ad329b2a31a502b1c5e42de9bae2304050444c6
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-04-22 08:51:10 +0700

    controller: add login logic to auth controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import * as Yup from "yup";
 import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
 
 type TRegister = {
     fullName: string;
@@ -8,6 +9,11 @@ type TRegister = {
     email: string;
     password: string;
     confirmPassword: string;
+};
+
+type TLogin = {
+    identifier: string;
+    password: string;
 };
 
 const registerValidateSchema = Yup.object({
@@ -38,14 +44,61 @@ export default {
                 fullName,
                 email,
                 username,
-                password
-            })
+                password,
+            });
 
             res.status(200).json({
                 message: "Success registration.",
-                data: result
+                data: result,
+            });
+        } catch (error) {
+            const err = error as unknown as Error;
+
+            res.status(400).json({
+                message: err.message,
+                data: null,
+            });
+        }
+    },
+
+    async login(req: Request, res: Response) {
+        const { identifier, password } = req.body as unknown as TLogin;
+
+        try {
+            // Get data user berdasarkan identifier (username dan email)
+            const userByIdentifier = await UserModel.findOne({
+                $or: [
+                    {
+                        email: identifier,
+                    },
+                    {
+                        username: identifier,
+                    },
+                ],
             });
 
+            if (!userByIdentifier) {
+                return res.status(403).json({
+                    message: "User not found",
+                    data: null,
+                });
+            }
+
+            // Validasi password
+            const validatePassword: boolean =
+                encrypt(password) === userByIdentifier.password;
+
+            if (!validatePassword) {
+                return res.status(403).json({
+                    message: "User not found",
+                    data: null,
+                });
+            }
+
+            res.status(200).json({
+                message: "Login successfully",
+                data: userByIdentifier,
+            });
         } catch (error) {
             const err = error as unknown as Error;
 
