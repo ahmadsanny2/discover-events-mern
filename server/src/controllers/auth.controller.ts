d9c8b3ae6bba105d7c29beeeac7cc00a5c8bfc9a
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-05-06 10:55:48 +0700

    feat(auth): implement user activation and enhanced validation

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -22,7 +22,31 @@ const registerValidateSchema = Yup.object({
     fullName: Yup.string().required(),
     username: Yup.string().required(),
     email: Yup.string().email().required(),
-    password: Yup.string().required(),
+    password: Yup.string()
+        .required()
+        .min(6, "Password must be at least 6 characters")
+        .test(
+            "at-least-one-uppercase-letter",
+            "Contains at least one uppercase letter",
+            (value) => {
+                if (!value) return false;
+
+                const regex = /^(?=.*[A-Z]).+$/;
+
+                return regex.test(value);
+            },
+        )
+        .test(
+            "at-least-one-uppercase-letter",
+            "Contains at least one number",
+            (value) => {
+                if (!value) return false;
+
+                const regex = /^(?=.*\d)/;
+
+                return regex.test(value);
+            },
+        ),
     confirmPassword: Yup.string()
         .required()
         .oneOf([Yup.ref("password"), ""], "Password doesn't match"),
@@ -30,6 +54,14 @@ const registerValidateSchema = Yup.object({
 
 export default {
     async register(req: Request, res: Response) {
+        /**
+            #swagger.tags = ['Auth']
+            #swagger.requestBody = {
+                required: true,
+                schema: {$ref: "#/components/schemas/RegisterRequest"}
+                      }
+                */
+
         const { fullName, username, email, password, confirmPassword } =
             req.body as unknown as TRegister;
 
@@ -65,10 +97,11 @@ export default {
 
     async login(req: Request, res: Response) {
         /**
-         #swagger.requestBody = {
-            required: true,
-            schema: {$ref: "#/components/schemas/LoginRequest"}
-         }
+             #swagger.tags = ['Auth']
+            #swagger.requestBody = {
+                  required: true,
+                  schema: {$ref: "#/components/schemas/LoginRequest"}
+               }
          */
 
         const { identifier, password } = req.body as unknown as TLogin;
@@ -84,6 +117,7 @@ export default {
                         username: identifier,
                     },
                 ],
+                isActive: true
             });
 
             if (!userByIdentifier) {
@@ -125,10 +159,11 @@ export default {
 
     async me(req: IReqUser, res: Response) {
         /**
-         #swagger.security = [{
-            "bearerAuth": []
-         }]
-         */
+            #swagger.tags = ['Auth']
+            #swagger.security = [{
+                "bearerAuth": []
+            }]
+        */
 
         try {
             const user = req.user;
@@ -138,6 +173,43 @@ export default {
             res.status(200).json({
                 message: "Success get user profile",
                 data: result,
+            });
+        } catch (error) {
+            const err = error as unknown as Error;
+
+            res.status(400).json({
+                message: err.message,
+                data: null,
+            });
+        }
+    },
+
+    async activation(req: Request, res: Response) {
+        /**
+             #swagger.tags = ['Auth']
+             #swagger.requestBody = {
+                required: true,
+                schema: {$ref: '#/components/schemas/ActivationRequest'}     
+             }
+             */
+
+        try {
+            const { code } = req.body as { code: string };
+
+            const user = await UserModel.findOneAndUpdate(
+                {
+                    activationCode: code,
+                },
+                {
+                    isActive: true,
+                },
+                {
+                    new: true,
+                },
+            );
+
+            res.status(200).json({
+                message: "User successfully activated",
             });
         } catch (error) {
             const err = error as unknown as Error;
