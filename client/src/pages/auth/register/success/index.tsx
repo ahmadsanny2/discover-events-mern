32c9a9a572e9e9d8479a5c171d2f27a1566863ac
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-05-25 10:37:13 +0700

    feat(client): add register success page view

---
+++ client/src/pages/auth/register/success/index.tsx
@@ -0,0 +1,11 @@
+import AuthLayout from "@/components/layouts/AuthLayout";
+
+const RegisterSuccessPage = () => {
+    return (
+        <AuthLayout title="DEM | Register Success">
+            <h1 className="">Register Success</h1>
+        </AuthLayout>
+    );
+};
+
+export default RegisterSuccessPage;
