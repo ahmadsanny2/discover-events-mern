ec8c99cff7b7e0e5fb4f54e6085a74d9254d937f
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-06-02 18:51:23 +0700

    style(client): adjust heading size and spacing in Register component

--- client/src/components/views/Auth/Register/Register.tsx
+++ client/src/components/views/Auth/Register/Register.tsx
@@ -37,8 +37,8 @@ const Register = () => {
 
             <Card>
                 <CardBody className="p-8">
-                    <h2 className="text-xl font-bold text-danger-500">Create Account</h2>
-                    <p className="mb-4 text-small">
+                    <h2 className="text-2xl font-bold text-danger-500">Create Account</h2>
+                    <p className="mb-4 mt-2 text-small">
                         Have an account? &nbsp;
                         <Link className="font-semibold text-danger-400" href="/auth/login">
                             Login here!
