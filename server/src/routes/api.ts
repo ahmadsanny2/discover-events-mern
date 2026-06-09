2734c9416d6a52930159dd350838b7f2ab62ddd4
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-06-09 14:12:15 +0700

    feat(server): add test-acl route with role-based authorization

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,12 +1,26 @@
-import express from "express";
+import express, { Request, Response } from "express";
+
+import { ROLES } from "../utils/constant";
+import aclMiddleware from "../middlewares/acl.middleware";
 import authController from "../controllers/auth.controller";
-import authMiddleware from "../middlewares/auth.middleware"
+import authMiddleware from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.post("/auth/register", authController.register);
 router.post("/auth/login", authController.login);
-router.get("/auth/me", authMiddleware, authController.me)
-router.post("/auth/activation", authController.activation)
+router.get("/auth/me", authMiddleware, authController.me);
+router.post("/auth/activation", authController.activation);
+
+router.get(
+    "/test-acl",
+    [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])],
+    (req: Request, res: Response) => {
+        res.status(200).json({
+            data: "success",
+            message: "OK",
+        });
+    },
+);
 
 export default router;
