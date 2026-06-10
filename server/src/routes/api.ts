8123987c186daabdf41174723413e4183b7ec5d3
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-06-10 08:49:57 +0700

    feat(server): add media upload and deletion routes

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,26 +1,42 @@
-import express, { Request, Response } from "express";
-
 import { ROLES } from "../utils/constant";
 import aclMiddleware from "../middlewares/acl.middleware";
 import authController from "../controllers/auth.controller";
 import authMiddleware from "../middlewares/auth.middleware";
+import express from "express";
+import mediaController from "../controllers/media.controller";
+import mediaMiddleware from "../middlewares/media.middleware";
 
 const router = express.Router();
 
+// Auth
 router.post("/auth/register", authController.register);
 router.post("/auth/login", authController.login);
 router.get("/auth/me", authMiddleware, authController.me);
 router.post("/auth/activation", authController.activation);
 
-router.get(
-    "/test-acl",
+// Media
+router.post(
+    "/media/upload-single",
+    [
+        authMiddleware,
+        aclMiddleware([ROLES.ADMIN, ROLES.MEMBER]),
+        mediaMiddleware.single("file"),
+    ],
+    mediaController.single,
+);
+router.post(
+    "/media/upload-multiple",
+    [
+        authMiddleware,
+        aclMiddleware([ROLES.ADMIN, ROLES.MEMBER]),
+        mediaMiddleware.multiple("files"),
+    ],
+    mediaController.multiple,
+);
+router.delete(
+    "/media/remove",
     [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])],
-    (req: Request, res: Response) => {
-        res.status(200).json({
-            data: "success",
-            message: "OK",
-        });
-    },
+    mediaController.remove,
 );
 
 export default router;
