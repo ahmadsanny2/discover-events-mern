f0dba473018ef36d00e6fc40ec7c72659d6f8816
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-06-11 13:27:08 +0700

    feat(server): add Category mongoose model and validation schema

---
+++ server/src/models/category.model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Mongoose } from "mongoose"
+import * as Yup from "yup"
+
+const Schema = mongoose.Schema
+
+export const categoryDAO = Yup.object({
+    name: Yup.string().required(),
+    description: Yup.string().required(),
+    icon: Yup.string()
+})
+
+export type Category = Yup.InferType<typeof categoryDAO>
+
+const categorySchema = new Schema<Category>({
+    name: {
+        type: Schema.Types.String,
+        required: true
+    },
+    description: {
+        type: Schema.Types.String,
+        required: true
+    },
+    icon: {
+        type: Schema.Types.String
+    }
+}, {
+    timestamps: true
+})
+
+const CategoryModel = mongoose.model("Category", categorySchema)
+
+export default CategoryModel
