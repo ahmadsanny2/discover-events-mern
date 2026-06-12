f13acf19ff0628667b40b85628fe0ae8074a3b0e
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-06-12 07:43:12 +0700

    feat(server): enhance API response utility error handling

--- server/src/utils/response.ts
+++ server/src/utils/response.ts
@@ -1,6 +1,7 @@
 import * as Yup from "yup"
 
 import { Response } from "express"
+import mongoose from "mongoose"
 
 type Pagination = {
     totalPages: number
@@ -26,9 +27,40 @@ export default {
                     status: 400,
                     message
                 },
-                data: error.errors
+                data: {
+                    [`${error.path}`]: error.errors
+                }
             })
         }
+
+        if (error instanceof mongoose.Error) {
+            return res.status(500).json({
+                meta: {
+                    status: 500,
+                    message: error.message
+                },
+                data: error.name
+            })
+        }
+
+        if ((error as any)?.code) {
+            const _err = error as any
+            return res.status(500).json({
+                meta: {
+                    status: 500,
+                    message: _err.errorResponse.errmsg
+                },
+                data: error
+            })
+        }
+
+        res.status(500).json({
+            meta: {
+                status: 500,
+                message: message
+            },
+            data: error
+        })
     },
 
     unauthorized(res: Response, message: string = "Unauthorized") {
