030f91d04af12a2f6a7e8b45cb9ec1e374152b2b
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-06-12 07:54:50 +0700

    style(response): format code and style cleanup

--- server/src/utils/response.ts
+++ server/src/utils/response.ts
@@ -1,23 +1,23 @@
-import * as Yup from "yup"
+import * as Yup from "yup";
 
-import { Response } from "express"
-import mongoose from "mongoose"
+import { Response } from "express";
+import mongoose from "mongoose";
 
 type Pagination = {
-    totalPages: number
-    current: number
-    total: number
-}
+    totalPages: number;
+    current: number;
+    total: number;
+};
 
 export default {
     success(res: Response, data: any, message: string) {
         res.status(200).json({
             meta: {
                 status: 200,
-                message
+                message,
             },
-            data
-        })
+            data,
+        });
     },
 
     error(res: Response, error: unknown, message: string) {
@@ -25,62 +25,67 @@ export default {
             return res.status(400).json({
                 meta: {
                     status: 400,
-                    message
+                    message,
                 },
                 data: {
-                    [`${error.path}`]: error.errors
-                }
-            })
+                    [`${error.path}`]: error.errors,
+                },
+            });
         }
 
         if (error instanceof mongoose.Error) {
             return res.status(500).json({
                 meta: {
                     status: 500,
-                    message: error.message
+                    message: error.message,
                 },
-                data: error.name
-            })
+                data: error.name,
+            });
         }
 
         if ((error as any)?.code) {
-            const _err = error as any
+            const _err = error as any;
             return res.status(500).json({
                 meta: {
                     status: 500,
-                    message: _err.errorResponse.errmsg
+                    message: _err.errorResponse.errmsg,
                 },
-                data: error
-            })
+                data: error,
+            });
         }
 
         res.status(500).json({
             meta: {
                 status: 500,
-                message: message
+                message: message,
             },
-            data: error
-        })
+            data: error,
+        });
     },
 
     unauthorized(res: Response, message: string = "Unauthorized") {
         res.status(403).json({
             meta: {
                 status: 403,
-                message
+                message,
             },
-            data: null
-        })
+            data: null,
+        });
     },
 
-    pagination(res: Response, data: any[], pagination: Pagination, message: string) {
+    pagination(
+        res: Response,
+        data: any[],
+        pagination: Pagination,
+        message: string,
+    ) {
         res.status(200).json({
             meta: {
                 status: 200,
-                message
+                message,
             },
             data,
-            pagination
-        })
-    }
-}
+            pagination,
+        });
+    },
+};
