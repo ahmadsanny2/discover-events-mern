ffae3b2aef53456deb28a670d3934e12acc58217
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-06-12 07:54:47 +0700

    style(jwt): format code and style cleanup

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -4,12 +4,12 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (user: IUserToken): string => {
     const token = jwt.sign(user, SECRET, {
-        expiresIn: "1h"
-    })
-    return token
+        expiresIn: "1h",
+    });
+    return token;
 };
 
 export const getUserData = (token: string) => {
-    const user = jwt.verify(token, SECRET) as IUserToken
-    return user
+    const user = jwt.verify(token, SECRET) as IUserToken;
+    return user;
 };
