46b3e557f3e9c74141d2ae9c2e764b5a21ff534e
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-06-12 07:54:20 +0700

    style(auth-middleware): format code and style cleanup

--- server/src/middlewares/auth.middleware.ts
+++ server/src/middlewares/auth.middleware.ts
@@ -5,25 +5,25 @@ import { getUserData } from "../utils/jwt";
 import response from "../utils/response";
 
 export default (req: Request, res: Response, next: NextFunction) => {
-    const authorization = req.headers?.authorization
+    const authorization = req.headers?.authorization;
 
     if (!authorization) {
-        return response.unauthorized(res)
+        return response.unauthorized(res);
     }
 
-    const [prefix, token] = authorization.split(" ")
+    const [prefix, token] = authorization.split(" ");
 
     if (!(prefix === "Bearer" && token)) {
-        return response.unauthorized(res)
+        return response.unauthorized(res);
     }
 
-    const user = getUserData(token)
+    const user = getUserData(token);
 
     if (!user) {
-        return response.unauthorized(res)
+        return response.unauthorized(res);
     }
 
-    (req as IReqUser).user = user
+    (req as IReqUser).user = user;
 
-    next()
-}
+    next();
+};
