1957b5eb9c1674c097b040a0d1362b5ab3c94b7a
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-06-15 09:17:00 +0700

    refactor(client): format dashboard layout and use onPress for menu toggle

--- client/src/components/layouts/DashboardLayout/DashboardLayout.tsx
+++ client/src/components/layouts/DashboardLayout/DashboardLayout.tsx
@@ -5,45 +5,45 @@ import { SIDEBAR_ADMIN, SIDEBAR_MEMBER } from "./DashboardLayout.constants";
 import { Navbar, NavbarMenuToggle } from "@nextui-org/react";
 
 interface PropTypes {
-    children: ReactNode;
-    description?: string;
-    title?: string;
-    type?: string;
+  children: ReactNode;
+  description?: string;
+  title?: string;
+  type?: string;
 }
 
 const DashboardLayout = (props: PropTypes) => {
-    const { children, description, title, type = "admin" } = props;
-    const [open, setOpen] = useState(false);
+  const { children, description, title, type = "admin" } = props;
+  const [open, setOpen] = useState(false);
 
-    return (
-        <>
-            <PageHead title={title} />
-            <div className="max-w-screen-3xl 3xl:container flex">
-                <DashboardLayoutSidebar
-                    sidebarItems={type === "admin" ? SIDEBAR_ADMIN : SIDEBAR_MEMBER}
-                    isOpen={open}
-                />
-                <div className="h-screen w-full overscroll-y-auto p-8">
-                    <Navbar
-                        className="flex justify-between bg-transparent px-0"
-                        isBlurred={false}
-                        position="static"
-                        classNames={{ wrapper: "p-0" }}
-                    >
-                        <h1 className="text-3xl font-bold">{title}</h1>
+  return (
+    <>
+      <PageHead title={title} />
+      <div className="max-w-screen-3xl 3xl:container flex">
+        <DashboardLayoutSidebar
+          sidebarItems={type === "admin" ? SIDEBAR_ADMIN : SIDEBAR_MEMBER}
+          isOpen={open}
+        />
+        <div className="h-screen w-full overscroll-y-auto p-8">
+          <Navbar
+            className="flex justify-between bg-transparent px-0"
+            isBlurred={false}
+            position="static"
+            classNames={{ wrapper: "p-0" }}
+          >
+            <h1 className="text-3xl font-bold">{title}</h1>
 
-                        <NavbarMenuToggle
-                            aria-label={open ? "Close Menu" : "Open Menu"}
-                            onClick={() => setOpen(!open)}
-                            className="lg:hidden"
-                        />
-                    </Navbar>
-                    <p className="mb-4 text-small">{description}</p>
-                    {children}
-                </div>
-            </div>
-        </>
-    );
+            <NavbarMenuToggle
+              aria-label={open ? "Close Menu" : "Open Menu"}
+              onPress={() => setOpen(!open)}
+              className="lg:hidden"
+            />
+          </Navbar>
+          <p className="mb-4 text-small">{description}</p>
+          {children}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DashboardLayout;
