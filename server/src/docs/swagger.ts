5a12c8915359f0ac6c06b3f348968315d0bd8b2b
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-06-15 08:33:47 +0700

    feat(server): add new request schemas to swagger configuration

--- server/src/docs/swagger.ts
+++ server/src/docs/swagger.ts
@@ -37,6 +37,28 @@ const doc = {
             },
             ActivationRequest: {
                 code: "abcde"
+            },
+            CreateCategoryRequest: {
+                name: "",
+                description: "",
+                icon: ""
+            },
+            CreateEventRequest: {
+                name: "",
+                banner: "fileUrl",
+                category: "category ObjectId",
+                description: "",
+                startDate: "yyyy-mm-dd hh:mm:ss",
+                endDate: "yyyy-mm-dd hh:mm:ss",
+                location: {
+                    region: "regionId",
+                    coordinates: [0, 0]
+                },
+                isOnline: false,
+                isFeatured: false
+            },
+            RemoveMediaRequest: {
+                fileUrl: ""
             }
         }
     }
