016cd0051c4017aadf72aef5db840424f5447504
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-07-16 15:04:42 +0700

    feat: update dashboard layout and sidebar link navigation

--- client/src/components/layouts/DashboardLayout/DashboardLayoutSidebar/DashboardLayoutSidebar.tsx
+++ client/src/components/layouts/DashboardLayout/DashboardLayoutSidebar/DashboardLayoutSidebar.tsx
@@ -2,7 +2,9 @@ import { cn } from "@/utils/cn";
 import { Button, Listbox, ListboxItem } from "@nextui-org/react";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/router";
+import { JSX } from "react";
 import { CiLogout } from "react-icons/ci";
 
 interface SidebarItem {
@@ -58,6 +60,8 @@ const DashboardLayoutSidebar = (props: PropTypes) => {
               textValue={item.label}
               aria-labelledby={item.label}
               aria-describedby={item.label}
+              as={Link}
+              href={item.href}
             >
               <p className="text-small">{item.label}</p>
             </ListboxItem>
