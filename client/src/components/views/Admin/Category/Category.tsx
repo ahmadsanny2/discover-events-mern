a76ba257f04d247079c8391ae3c650b50237a1bd
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-08-01 15:06:11 +0700

    refactor(client): update Category view to use useCategory hook and DataTable

--- client/src/components/views/Admin/Category/Category.tsx
+++ client/src/components/views/Admin/Category/Category.tsx
@@ -8,23 +8,45 @@ import {
 } from "@nextui-org/react";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { Key, ReactNode, useCallback } from "react";
+import { Key, ReactNode, useCallback, useEffect } from "react";
 import { COLUMN_LIST_CATEGORY } from "./Category.constant";
 import { CiMenuKebab } from "react-icons/ci";
-import { LIMIT_LISTS } from "@/constants/lists.constants";
+import useCategory from "./useCategory";
 
 const Category = () => {
-    const { push } = useRouter();
+    const { push, isReady, query } = useRouter();
+
+    const {
+        dataCategory,
+        isLoadingCategory,
+        isRefetchingCategory,
+
+        currentPage,
+        currentLimit,
+        handleChangeLimit,
+        handleChangePage,
+        handleSearch,
+        handleClearSearch,
+        setURL,
+    } = useCategory();
+
+    console.log(dataCategory);
+
+    useEffect(() => {
+        if (isReady) {
+            setURL();
+        }
+    }, [isReady]);
 
     const renderCell = useCallback(
         (category: Record<string, unknown>, columnKey: Key) => {
             const cellValue = category[columnKey as keyof typeof category];
 
             switch (columnKey) {
-                case "icon":
-                    return (
-                        <Image src={`${cellValue}`} alt="icon" width={100} height={200} />
-                    );
+                // case "icon":
+                //     return (
+                //         <Image src={`${cellValue}`} alt="icon" width={100} height={200} />
+                //     );
                 case "actions":
                     return (
                         <Dropdown>
@@ -55,28 +77,24 @@ const Category = () => {
 
     return (
         <section>
-            <DataTable
-                buttonTopContentLabel="Create Category"
-                columns={COLUMN_LIST_CATEGORY}
-                currentPage={1}
-                data={[
-                    {
-                        _id: "123",
-                        name: "Category 1",
-                        description: "Lorem ipsum",
-                        icon: "/images/general/logo.png",
-                    },
-                ]}
-                emptyContent="Category is empty"
-                limit={LIMIT_LISTS[0].label}
-                onChangeLimit={() => { }}
-                onChangePage={() => { }}
-                onChangeSearch={() => { }}
-                onClearSearch={() => { }}
-                onClickButtonTopContent={() => { }}
-                renderCell={renderCell}
-                totalPages={2}
-            />
+            {Object.keys(query).length > 0 && (
+                <DataTable
+                    buttonTopContentLabel="Create Category"
+                    columns={COLUMN_LIST_CATEGORY}
+                    currentPage={Number(currentPage)}
+                    data={dataCategory?.data || []}
+                    emptyContent="Category is empty"
+                    isLoading={isLoadingCategory || isRefetchingCategory}
+                    limit={String(currentLimit)}
+                    onChangeLimit={handleChangeLimit}
+                    onChangePage={handleChangePage}
+                    onChangeSearch={handleSearch}
+                    onClearSearch={handleClearSearch}
+                    onClickButtonTopContent={() => { }}
+                    renderCell={renderCell}
+                    totalPages={dataCategory?.pagination.totalPages}
+                />
+            )}
         </section>
     );
 };
