5d3f104dd9f8bfb90b606ac05fcadb3c8008ec33
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-08-01 15:06:08 +0700

    feat(client): add pagination and debounce delay constants

--- client/src/constants/lists.constants.ts
+++ client/src/constants/lists.constants.ts
@@ -1,16 +1,20 @@
 const LIMIT_LISTS = [
     {
         label: "8",
-        value: 8
+        value: 8,
     },
     {
         label: "12",
-        value: 12
+        value: 12,
     },
     {
         label: "16",
-        value: 16
+        value: 16,
     },
-]
+];
 
-export { LIMIT_LISTS }
+const LIMIT_DEFAULT = LIMIT_LISTS[0].value;
+const PAGE_DEFAULT = 1;
+const DELAY = 500;
+
+export { DELAY, LIMIT_LISTS, LIMIT_DEFAULT, PAGE_DEFAULT };
