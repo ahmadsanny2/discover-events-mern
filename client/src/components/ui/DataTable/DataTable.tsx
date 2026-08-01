2c1828f0d98c715843339c07cccac75fc80062b4
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-08-01 15:06:13 +0700

    refactor(client): improve DataTable pagination visibility and layout padding

--- client/src/components/ui/DataTable/DataTable.tsx
+++ client/src/components/ui/DataTable/DataTable.tsx
@@ -79,7 +79,7 @@ const DataTable = (props: PropTypes) => {
 
     const BottomContent = useMemo(() => {
         return (
-            <div className="flex items-center justify-center p-2 lg:justify-between">
+            <div className="flex items-center justify-center lg:justify-between">
                 <Select
                     className="hidden max-w-36 lg:block"
                     size="md"
@@ -87,6 +87,7 @@ const DataTable = (props: PropTypes) => {
                     selectionMode="single"
                     onChange={onChangeLimit}
                     startContent={<p className="text-small">Show:</p>}
+                    disallowEmptySelection
                 >
                     {LIMIT_LISTS.map((item) => (
                         <SelectItem key={item.value} value={item.value}>
@@ -94,14 +95,17 @@ const DataTable = (props: PropTypes) => {
                         </SelectItem>
                     ))}
                 </Select>
-                <Pagination
-                    isCompact
-                    showControls
-                    color="danger"
-                    page={currentPage}
-                    total={totalPages}
-                    onChange={onChangePage}
-                />
+                {totalPages > 1 && (
+                    <Pagination
+                        isCompact
+                        showControls
+                        color="danger"
+                        page={currentPage}
+                        total={totalPages}
+                        onChange={onChangePage}
+                        loop
+                    />
+                )}
             </div>
         );
     }, [currentPage, limit, onChangeLimit, onChangePage, totalPages]);
@@ -111,7 +115,7 @@ const DataTable = (props: PropTypes) => {
             bottomContentPlacement="outside"
             classNames={{
                 base: "max-w-full",
-                wrapper: cn({ "overflow-x-hidden": isLoading })
+                wrapper: cn({ "overflow-x-hidden": isLoading }),
             }}
             topContent={TopContent}
             topContentPlacement="outside"
