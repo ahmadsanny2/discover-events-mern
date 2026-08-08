c4859a2868553e4a562b3d53555591d09aaf7652
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-08-08 18:11:04 +0700

    refactor(auth): implement toaster for register notifications

--- client/src/components/views/Auth/Register/useRegister.ts
+++ client/src/components/views/Auth/Register/useRegister.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -6,6 +6,7 @@ import { IRegister } from "@/types/Auth";
 import authServices from "@/services/auth.service";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/router";
+import { ToasterContext } from "@/contexts/ToasterContext";
 
 const registerSchema = yup.object().shape({
     fullName: yup.string().required("Please input your full name"),
@@ -26,6 +27,7 @@ const registerSchema = yup.object().shape({
 
 const useRegister = () => {
     const router = useRouter();
+    const { setToaster } = useContext(ToasterContext);
 
     const [visiblePassword, setVisiblePassword] = useState({
         password: false,
@@ -38,6 +40,7 @@ const useRegister = () => {
             [key]: !visiblePassword[key],
         });
     };
+
 
     const {
         control,
@@ -56,14 +59,19 @@ const useRegister = () => {
 
     const { mutate: mutateRegister, isPending: isPendingRegister } = useMutation({
         mutationFn: registerService,
-        onError(error) {
-            setError("root", {
+        onError: (error) => {
+            setToaster({
+                type: "error",
                 message: error.message,
             });
         },
         onSuccess: () => {
-            router.push("/auth/register/success");
             reset();
+            setToaster({
+                type: "success",
+                message: "Register successfully",
+            });
+            router.push("/auth/register/success");
         },
     });
 
@@ -76,7 +84,7 @@ const useRegister = () => {
         handleSubmit,
         handleRegister,
         isPendingRegister,
-        errors
+        errors,
     };
 };
 
