3850b701fda61d7db56c60e11eeb1fcabf2b5242
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-09-01 18:59:42 +0700

    feat(client): add DeleteCategoryModal component and hook

---
+++ client/src/components/views/Admin/Category/DeleteCategoryModal/useDeleteCategoryModal.tsx
@@ -0,0 +1,42 @@
+import { ToasterContext } from "@/contexts/ToasterContext";
+import categoryServices from "@/services/caategory.service";
+import { useMutation } from "@tanstack/react-query";
+import { useContext } from "react";
+
+const useDeleteCategoryModal = () => {
+    const { setToaster } = useContext(ToasterContext);
+
+    const deleteCategory = async (id: string) => {
+        const res = await categoryServices.deleteCategory(id);
+
+        return res;
+    };
+
+    const {
+        mutate: mutateDeleteCategory,
+        isPending: isPendingMutateDeleteCategory,
+        isSuccess: isSuccessMutateDeleteCategory,
+    } = useMutation({
+        mutationFn: deleteCategory,
+        onSuccess: () => {
+            setToaster({
+                type: "success",
+                message: "Category deleted successfully",
+            });
+        },
+        onError: (error) => {
+            setToaster({
+                type: "error",
+                message: error.message,
+            });
+        },
+    });
+
+    return {
+        mutateDeleteCategory,
+        isPendingMutateDeleteCategory,
+        isSuccessMutateDeleteCategory,
+    };
+};
+
+export default useDeleteCategoryModal;
