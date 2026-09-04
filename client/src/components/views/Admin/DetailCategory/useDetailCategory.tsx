4f5af6b796d898aac5db989d4ca6b318782eabc9
ahmadsanny02 <ahmadsanijabarulloh.02@gmail.com>
2026-09-04 21:38:16 +0700

    feat(client): add update category mutation in useDetailCategory hook

--- client/src/components/views/Admin/DetailCategory/useDetailCategory.tsx
+++ client/src/components/views/Admin/DetailCategory/useDetailCategory.tsx
@@ -1,25 +1,69 @@
-import categoryServices from "@/services/caategory.service"
-import { useQuery } from "@tanstack/react-query"
-import { useRouter } from "next/router"
+import { ToasterContext } from "@/contexts/ToasterContext";
+import categoryServices from "@/services/caategory.service";
+import { ICategory } from "@/types/Category";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { useContext } from "react";
 
 const useDetailCategory = () => {
-    const { query, isReady } = useRouter()
+    const { query, isReady } = useRouter();
+    const { setToaster } = useContext(ToasterContext);
+
+
 
     const getCategoryById = async (id: string) => {
-        const { data } = await categoryServices.getCategoryById(id)
+        const { data } = await categoryServices.getCategoryById(id);
 
-        return data.data
-    }
+        return data.data;
+    };
 
-    const { data: dataCategory } = useQuery({
+    const { data: dataCategory, refetch: refetchCategory } = useQuery({
         queryKey: ["Category"],
         queryFn: () => getCategoryById(`${query.id}`),
-        enabled: isReady
-    })
+        enabled: isReady,
+    });
+
+    const updateCategory = async (payload: ICategory) => {
+        const { data } = await categoryServices.updateCategory(
+            `${query.id}`,
+            payload,
+        );
+
+        return data.data;
+    };
+
+    const {
+        mutate: mutateUpdateCategory,
+        isPending: isPendingMutateUpdateCategory,
+        isSuccess: isSuccessMutateUpdateCategory,
+    } = useMutation({
+        mutationFn: (payload: ICategory) => updateCategory(payload),
+        onError: (error) => {
+            setToaster({
+                type: "error",
+                message: error.message,
+            });
+        },
+        onSuccess: () => {
+            refetchCategory();
+            setToaster({
+                type: "success",
+                message: "Success update category",
+            });
+        },
+    });
+
+    const handleUpdateCategory = (data: ICategory) => mutateUpdateCategory(data)
+
+
 
     return {
-        dataCategory
-    }
-}
+        dataCategory,
 
-export default useDetailCategory
+        handleUpdateCategory,
+        isPendingMutateUpdateCategory,
+        isSuccessMutateUpdateCategory,
+    };
+};
+
+export default useDetailCategory;
